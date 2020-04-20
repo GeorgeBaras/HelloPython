24befb7f7d7488312c93501845f03b48122d01c9
George <georgebaras@outlook.com>
2020-04-20 10:51:11 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,7 @@
+character_name = "John"
+character_age = 35
+is_male = False
+print("There was a man called "+character_name+", ")
+print("he was "+character_age+" years old")
+print("He really liked the name "+character_name)
+print("not so much being "+character_age+" though")
